1bc194fbd5077ed193e9062564353aa1e4c7561e
KRISHNA KARANWAL <krishnakaranwal@gmail.com>
2025-12-08 01:03:14 +0530

    Vercel to Render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://donation-management-fi79bcdhb-krishnas-projects-6ad65eb3.vercel.app";
+export const API_BASE_URL = "https://donation-management-59d6.onrender.com";
